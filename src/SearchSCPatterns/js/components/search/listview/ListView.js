24b8a91006349fd3de03e992a4a67341b3bacad6
Akshay Uday <auakshay123@gmail.com>
2017-04-20 11:48:13 -0400

    Update ListView.js

--- src/SearchSCPatterns/js/components/search/listview/ListView.js
+++ src/SearchSCPatterns/js/components/search/listview/ListView.js
@@ -1,35 +1,80 @@
 import React, {Component} from 'react';
-import ListColumn from './ListColumns'
-import ListRow from './ListRows'
-import PL_Scroll from '../../../../../common/components/PL_ScrollBar'
 import ListStyles from './ListViewStyles.css'
-const ListView = (props) => {
-	
-		// let area = props.flag ? ListStyles.filterArea : ListStyles.area;
-		return(
-			<div className={ListStyles.listViewTable}>
-				<ListColumn columns={props.columns}
-				            onColumnSort={props.onColumnSort}
-				            columnsort = {props.columnsort}
-				            clickedItem = {props.clickedItem}/>
-			<PL_Scroll classname={ListStyles.area}>
-				<ListRow rows={props.rows} 
-						 zibraRows={props.zibraRows}
-						 radioHandler={props.radioHandler}/>
-			</PL_Scroll>
-			</div>
-		)
-	
-	
+import RadioBtn from '../../../../../common/components/Radio'
+
+class ListRows extends Component{
+
+	constructor(props){ 
+		super(props);
+		this.setSelectedItem = this.setSelectedItem.bind(this);
+		this.state = { setSelectedItemId: ''};
+		
+	}
+	setSelectedItem(selectedItem){ 
+
+		this.props.radioHandler(selectedItem);
+		this.setState({setSelectedItemId:selectedItem.id});
+
+	}
+
+    assetSelectedEvent(me, isTrue=true) {
+	    if (isTrue) {
+	      this.customFn(this.props.record);
+	    }
+	    else {
+	      this.customFn({});
+	    }
+   }
+
+
+	render(){ 
+		let self = this;
+		let isZibra = this.props.zibraRows ? ListStyles.zibrarows : ListStyles.noZibraRows;
+		let rowContent = 'No Results Found';
+		if(this.props.rows.length > 0){
+			rowContent = this.props.rows.map(function (row){
+            let checked = false;
+		    
+		    if(self.state.setSelectedItemId == row.id && self.state.setSelectedItemId != ''){
+              checked = true;
+		    } 
+
+
+
+			return (
+					<div className={ListStyles.rowContent}>
+
+						<div className={ListStyles.rowTitleWrapper}>			
+							<div className={ListStyles.rowRadioBtn}>
+							<RadioBtn name='radiobtn' record={row} 
+									  parent={self.assetSelectedEvent}
+									  customFn={self.setSelectedItem} checked={checked}/>
+							</div>
+							<div className='rowTitle'>
+								{row.title}
+							</div>
+						</div>
+						<div className='rowType'>{row.type}</div>
+						<div className='rowDatemodified'>{row.dateModified}</div>
+					</div>
+				
+					)
+		});
+
+		}
+		
+
+		return(<div className={isZibra}>
+				{rowContent}
+				</div>
+			   )
+	}
 }
-ListView.propTypes = {
-	flag : React.PropTypes.bool,
-	columns: React.PropTypes.object,
+
+ListRows.propTypes = {
+	rows: React.PropTypes.array,
 	zibraRows: React.PropTypes.bool,
-	rows:React.PropTypes.array,
 	radioHandler: React.PropTypes.func,
-	onColumnSort: React.PropTypes.func,
-	columnsort: React.PropTypes.bool,
-	clickedItem: React.PropTypes.string
+	record : React.PropTypes.object
 }
-export default ListView;
+export default ListRows;
